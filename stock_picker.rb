0f25f3f2429513ee33b7da23f8ebb97be3d8e821
Eric M. Todd <eterictodd@gmail.com>
2021-08-04 16:50:27 -0600

    Initial commit.

---
+++ stock_picker.rb
@@ -0,0 +1,11 @@
+def stock_picker(array)
+  p(array)
+
+  # Step 1. Find the smallest number.
+  # Step 2. Find largest number after the smallest number.
+  # Step 3. Calculate the difference.
+  # Step 4. Repeat this process where the smallest number is the next smallest after the previous one.
+  # Step 5. Return indexes of the numbers with greatest difference.
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
