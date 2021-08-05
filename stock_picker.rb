062ead8700052bb003b850dd8544edc31dd2f375
Eric M. Todd <eterictodd@gmail.com>
2021-08-04 18:51:58 -0600

    Working on establishing a contrast to evaluate data against in order to find the greatest profit margins.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,39 @@
 def stock_picker(array)
   p(array)
 
+  min = {
+    value: 99,
+    index: 0
+  }
+
+  max = {
+    value: 0,
+    index: 0
+  }
+
+  difference = 0;
+
   # Step 1. Find the smallest number.
+  array.each_with_index do |number, index|
+    if number < min[:value]
+      min[:value] = number
+      min[:index] = index
+    end
+  end
+  p("min: #{min}")
+
   # Step 2. Find largest number after the smallest number.
+  array[min[:index]..-1].each_with_index do |number, index|
+    if number > max[:value]
+      max[:value] = number
+      max[:index] = index
+    end
+  end
+  p("max: #{max}")
+
   # Step 3. Calculate the difference.
+  difference = max[:value] - min[:value]
+  p(difference)
   # Step 4. Repeat this process where the smallest number is the next smallest after the previous one.
   # Step 5. Return indexes of the numbers with greatest difference.
 end
